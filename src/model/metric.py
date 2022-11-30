9c9d8dd4c6474858f924ba584267796569577f45
Allison Chen <allisonchen@princeton.edu>
2022-11-30 18:38:19 -0500

    making too many changes with metric -- failedddd

--- src/model/metric.py
+++ src/model/metric.py
@@ -21,6 +21,15 @@ def accuracy(output, target):
         correct += torch.sum(pred == target).item()
     return correct / len(target)
 
+def _accuracy(output, target):
+    print("helper acc")
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        correct = 0
+        correct += torch.sum(pred == target).item()
+    return torch.tensor([correct]), torch.tensor([len(target)])
+
 
 def top_k_acc(output, target, k=3):
     with torch.no_grad():
