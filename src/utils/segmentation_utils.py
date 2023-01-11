9ff92aaaf44428397541ce15352e1876c8aae318
Allison Chen <allisonchen@princeton.edu>
2023-01-11 18:33:35 -0500

    made segment_and_modify.py to run as a script

--- src/utils/segmentation_utils.py
+++ src/utils/segmentation_utils.py
@@ -3,6 +3,11 @@ import torch
 import skimage.segmentation as segmentation
 import skimage.filters as filters
 import skimage.color as color
+import sys
+
+sys.path.insert(0, 'src')
+import utils.visualizations as visualizations
+from utils import informal_log
 
 def segment(image, method, kwargs):
     '''
@@ -146,6 +151,184 @@ def modify_segments(image,
 
     return modified_images
 
+def segment_modify_multi_method(image, methods_params, seed, save_path=None):
+    segmentations = []
+    gaussian_modified_images = []
+    masked_modified_images = []
+
+    for method, params in methods_params:
+        segments = segment(
+            image,
+            method=method,
+            kwargs=params)
+        segmentations.append(segments)
+
+        # Modify segments with noise
+        gaussian_modified_images.append(modify_segments(
+            image,
+            segments=segments,
+            method='gaussian_noise',
+            mean=0,
+            std=0.1,
+            seed=seed))
+
+        # Modify segments with masking
+        masked_modified_images.append(modify_segments(
+            image=image,
+            segments=segments,
+            method='mask'))
+
+    save_data = {
+        "original_image": image,  # np.array
+        "segmentation_methods": methods_params,  # list[(str, dict)]
+        "segmentations": segmentations,  # list[np.array]
+        "gaussian_modified_images": gaussian_modified_images,  # list[list[np.array]]
+        "masked_modified_images": masked_modified_images  # list[list[np.array]]
+    }
+
+    if save_path is not None:
+        torch.save(save_data, save_path)
+    return save_data
+
+
+def predict_modified_segmentations(modified_segmentations,
+                                   model,
+                                   device,
+                                   target_class_idx,
+                                   log_path,
+                                   visualization_save_path=None):
+    # Unpack data
+    image = modified_segmentations['original_image']
+    image = np.expand_dims(image, axis=0)
+    image = torch.from_numpy(image).type(torch.FloatTensor).to(device)
+
+    methods = modified_segmentations['segmentation_methods']
+    all_gaussian_modified_images = modified_segmentations['gaussian_modified_images']
+    all_masked_modified_images = modified_segmentations['masked_modified_images']
+
+    # Create lists for storing logits
+    all_gaussian_modified_logits = []
+    all_masked_modified_logits = []
+
+    # First pass original image through model
+    with torch.no_grad():
+        original_logits = model(image)
+        original_logits = original_logits.cpu().numpy()
+
+    # Data structures for keeping track of successful modifications
+    success_modifications = []  # store images
+    success_names = []  # store names of successful images
+
+    # Pass images through model to get predictions
+    for idx, (method, _) in enumerate(methods):
+        # Index into current modified images
+        gaussian_modified_images = all_gaussian_modified_images[idx]
+        masked_modified_images = all_masked_modified_images[idx]
+        n_modified = len(gaussian_modified_images)
+
+        # Stack along batch dimension
+        all_images = np.stack(gaussian_modified_images + masked_modified_images, axis=0)
+        # Convert to tensor & switch to GPU
+        all_images = torch.from_numpy(all_images).type(torch.FloatTensor)
+        all_images = all_images.to(device)
+
+        # Forward through model
+        with torch.no_grad():
+            all_logits = model(all_images)
+
+        # Obtain indices where the prediction is the target
+        all_predictions = torch.argmax(all_logits, dim=1)
+        success_idxs = (all_predictions == target_class_idx).nonzero().squeeze(dim=-1)
+
+        # Store images in respective list
+        success_images = all_images[success_idxs]
+        # If only one image, expand dims to be 1 x C x H x W
+        if len(success_images.shape) == 3:
+            success_images = torch.unsqueeze(success_images, dim=0)
+        success_modifications.append(success_images)
+
+        # Store names of each image in respective list
+        if success_idxs.shape[0] != 0:
+            informal_log("{} segmentation had {} sucessful modification(s)...".format(method, success_idxs.shape[0]), log_path)
+            for idx in success_idxs:
+                if idx < n_modified:
+                    mod_type = 'gaussian'
+                else:
+                    mod_type = 'masked'
+                name = "{}_{}_{}.png".format(method, mod_type, idx % n_modified)
+                success_names.append(name)
+
+                informal_log("  {}".format(name), log_path)
+
+    # Concatenate into one tensor
+    success_modifications = torch.cat(success_modifications, dim=0).cpu().numpy()
+    print(success_modifications.shape)
+    if success_modifications.shape[0] == 0:
+        informal_log("No successful modifications produced...", log_path)
+        return success_modifications, success_names
+
+    # Visualize
+    if visualization_save_path is not None:
+        visualizations.show_image_rows(
+            images=[list(success_modifications)],
+            image_titles=[success_names],
+            figure_title='Correctly predicted')
+
+    return success_modifications, success_names
+
+
+def visualize_segmentations(image,
+                            methods,
+                            segmentations,
+                            gaussian_modified_images,
+                            masked_modified_images,
+                            save_path=None):
+    visualization_grid = []
+    row_labels = []
+    max_row_len = 0
+
+    # Iterate through all segmentation methods
+    for idx, (method, _) in enumerate(methods):
+        # Obtain current segmentation and overlay boundaries
+        cur_segmentation = segmentations[idx]
+        segmentation_overlay = segmentation.mark_boundaries(np.transpose(image, axes=[1, 2, 0]), cur_segmentation)
+
+        # Obtain Gaussian and masked modifications
+        gaussian_modified_row = gaussian_modified_images[idx]
+        masked_modified_row = masked_modified_images[idx]
+
+        # Top row will be for Gaussian
+        row_labels.append(method + ' Gaussian')
+        row_images = [segmentation_overlay] + gaussian_modified_row
+        visualization_grid.append(row_images)
+
+        # Bottom row will be for masked
+        row_labels.append(method + ' Masked')
+        row_images = [segmentation_overlay] + masked_modified_row
+        visualization_grid.append(row_images)
+
+        # Update max_row_len for display purposes
+        if len(row_images) > max_row_len:
+            max_row_len = len(row_images)
+
+    # Pad extra columns with None
+    for idx, row in enumerate(visualization_grid):
+        n_pad = max_row_len - len(row)
+        padding = [None for i in range(n_pad)]
+        visualization_grid[idx] = row + padding
+
+    # Sanity check
+    for row in visualization_grid:
+        assert len(row) == max_row_len
+
+    # Save path
+    # visualization_save_path = os.path.join(save_dir, 'segment-visualizations.png')
+    visualizations.show_image_rows(
+        images=visualization_grid,
+        row_labels=row_labels,
+        save_path=save_path)
+    if save_path is not None:
+        print("Saved visualization to {}".format(save_path))
 
 def calculate_change(anchor, data, abs_val=True):
     '''
